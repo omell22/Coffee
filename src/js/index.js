b5fe1f4365bdce5aac69e2781d123e80ab415775
jullis2003 <you@example.com>
2023-09-01 20:42:36 +0300

    new comm

--- src/js/index.js
+++ src/js/index.js
@@ -1,15 +1,28 @@
-const readMoreButtons = document.querySelectorAll('.catalog__read-more');
-const itemsContainers = document.querySelectorAll('.catalog__items.catalog__items-hidden');
+const arrowUp = document.querySelector(".arrow-up");
+
+console.log(arrowUp);
+
+// window.addEventListener("scroll", () => {
+//   if (window.scrollY > 200) {
+//     // Измените значение 200 на то, при скольких пикселях от верха страницы вы хотите изменить цвет
+//     arrowUp.style.backgroundColor = "red"; // Измените цвет на желаемый
+//   } else {
+//     arrowUp.style.backgroundColor = "blue"; // Вернуть начальный цвет
+//   }
+// });
+
+const readMoreButtons = document.querySelectorAll(".catalog__read-more");
+const itemsContainers = document.querySelectorAll(
+  ".catalog__items.catalog__items-hidden"
+);
 // console.log(itemsContainers[1]);
 
 readMoreButtons.forEach(function (button, index) {
-   button.addEventListener('click', function () {
-      event.preventDefault();
-      itemsContainers[index].classList.toggle('catalog__items-hidden');
-   });
+  button.addEventListener("click", function () {
+    event.preventDefault();
+    itemsContainers[index].classList.toggle("catalog__items-hidden");
+  });
 });
-
-
 
 const close = document.querySelector(".cookies__close");
 const submit = document.querySelector(".cookies__btn");
@@ -18,70 +31,62 @@ close.addEventListener("click", closeCookie);
 submit.addEventListener("click", closeCookie);
 
 function closeCookie() {
-   document.querySelector(".cookies").style.display = "none";
+  document.querySelector(".cookies").style.display = "none";
 }
 
-
-const contentImgBlock = document.querySelector('.about__img');
-const contentImg = contentImgBlock.querySelector('img');
+const contentImgBlock = document.querySelector(".about__img");
+const contentImg = contentImgBlock.querySelector("img");
 const content = document.querySelector(".about__text");
 
 const maxHeight = contentImg.scrollHeight * 0.85;
-const words = content.textContent.split(' ');
-let newText = '';
+const words = content.textContent.split(" ");
+let newText = "";
 
 for (const word of words) {
-   const testText = newText + ' ' + word;
-   content.textContent = testText;
-   if (content.scrollHeight <= maxHeight) {
-      newText = testText;
-   } else {
-      content.textContent = newText + '...';
-      break;
-   }
+  const testText = newText + " " + word;
+  content.textContent = testText;
+  if (content.scrollHeight <= maxHeight) {
+    newText = testText;
+  } else {
+    content.textContent = newText + "...";
+    break;
+  }
 }
 
-
-
-const contentImgBlock2 = document.querySelector('.description__img');
-const contentImg2 = contentImgBlock.querySelector('img');
+const contentImgBlock2 = document.querySelector(".description__img");
+const contentImg2 = contentImgBlock.querySelector("img");
 const content2 = document.querySelector(".description__text");
 
 const maxHeight2 = contentImg.scrollHeight * 0.95;
-const words2 = content.textContent.split(' ');
-let newText2 = '';
+const words2 = content.textContent.split(" ");
+let newText2 = "";
 
 for (const word2 of words2) {
-   const testText2 = newText2 + ' ' + word2;
-   content2.textContent = testText2;
-   if (content2.scrollHeight <= maxHeight2) {
-      newText2 = testText2;
-   } else {
-      content2.textContent = newText2 + '...';
-      break;
-   }
+  const testText2 = newText2 + " " + word2;
+  content2.textContent = testText2;
+  if (content2.scrollHeight <= maxHeight2) {
+    newText2 = testText2;
+  } else {
+    content2.textContent = newText2 + "...";
+    break;
+  }
 }
 
-
-
-
-
-
-const menu = document.querySelector('.menu')
-const burger = document.querySelector('.header__burger');
-burger.addEventListener('click', showBurger);
-const body = document.querySelector('body');
+const menu = document.querySelector(".menu");
+const burger = document.querySelector(".header__burger");
+burger.addEventListener("click", showBurger);
+const body = document.querySelector("body");
 function showBurger(event) {
-   burger.classList.toggle('active');
-   menu.classList.toggle('active');
+  burger.classList.toggle("active");
+  menu.classList.toggle("active");
 }
 
-const img = document.querySelector('.menu__mobile-search');
-const search = document.querySelector('.menu__search');
-img.addEventListener('click', showSearch);
+const img = document.querySelector(".menu__mobile-search");
+const search = document.querySelector(".menu__search");
+img.addEventListener("click", showSearch);
 //search.addEventListener('click', showSearch);
 // const body = document.querySelector('body');
 function showSearch(event) {
-   //img.classList.toggle('active');
-   search.classList.toggle('active');
+  //img.classList.toggle('active');
+  search.classList.toggle("active");
 }
