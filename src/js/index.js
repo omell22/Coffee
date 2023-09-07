f459b12bc748a227d71ac0262c0bebddf0d94efd
jullis2003 <lisunjulia@gmail.com>
2023-09-07 21:40:19 +0300

    change line-height

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,14 @@
-//const arrowUp = document.querySelector(".arrow-up");
-
-//console.log(arrowUp);
-
-// window.addEventListener("scroll", () => {
-//   if (window.scrollY > 200) {
-//     // Измените значение 200 на то, при скольких пикселях от верха страницы вы хотите изменить цвет
-//     arrowUp.style.backgroundColor = "red"; // Измените цвет на желаемый
-//   } else {
-//     arrowUp.style.backgroundColor = "blue"; // Вернуть начальный цвет
-//   }
-// });
-
 const readMoreButtons = document.querySelectorAll(".catalog__read-more");
 const itemsContainers = document.querySelectorAll(
-   ".catalog__items.catalog__items-hidden"
+  ".catalog__items.catalog__items-hidden"
 );
 // console.log(itemsContainers[1]);
 
 readMoreButtons.forEach(function (button, index) {
-   button.addEventListener("click", function () {
-      event.preventDefault();
-      itemsContainers[index].classList.toggle("catalog__items-hidden");
-   });
+  button.addEventListener("click", function () {
+    event.preventDefault();
+    itemsContainers[index].classList.toggle("catalog__items-hidden");
+  });
 });
 
 const close = document.querySelector(".cookies__close");
@@ -31,7 +18,7 @@ close.addEventListener("click", closeCookie);
 submit.addEventListener("click", closeCookie);
 
 function closeCookie() {
-   document.querySelector(".cookies").style.display = "none";
+  document.querySelector(".cookies").style.display = "none";
 }
 
 const contentImgBlock = document.querySelector(".about__img");
@@ -43,14 +30,14 @@ const words = content.textContent.split(" ");
 let newText = "";
 
 for (const word of words) {
-   const testText = newText + " " + word;
-   content.textContent = testText;
-   if (content.scrollHeight <= maxHeight) {
-      newText = testText;
-   } else {
-      content.textContent = newText + "...";
-      break;
-   }
+  const testText = newText + " " + word;
+  content.textContent = testText;
+  if (content.scrollHeight <= maxHeight) {
+    newText = testText;
+  } else {
+    content.textContent = newText + "...";
+    break;
+  }
 }
 
 const contentImgBlock2 = document.querySelector(".description__img");
@@ -62,14 +49,14 @@ const words2 = content.textContent.split(" ");
 let newText2 = "";
 
 for (const word2 of words2) {
-   const testText2 = newText2 + " " + word2;
-   content2.textContent = testText2;
-   if (content2.scrollHeight <= maxHeight2) {
-      newText2 = testText2;
-   } else {
-      content2.textContent = newText2 + "...";
-      break;
-   }
+  const testText2 = newText2 + " " + word2;
+  content2.textContent = testText2;
+  if (content2.scrollHeight <= maxHeight2) {
+    newText2 = testText2;
+  } else {
+    content2.textContent = newText2 + "...";
+    break;
+  }
 }
 
 const menu = document.querySelector(".menu");
@@ -77,8 +64,8 @@ const burger = document.querySelector(".header__burger");
 burger.addEventListener("click", showBurger);
 const body = document.querySelector("body");
 function showBurger(event) {
-   burger.classList.toggle("active");
-   menu.classList.toggle("active");
+  burger.classList.toggle("active");
+  menu.classList.toggle("active");
 }
 
 const img = document.querySelector(".menu__mobile-search");
@@ -87,34 +74,26 @@ img.addEventListener("click", showSearch);
 //search.addEventListener('click', showSearch);
 // const body = document.querySelector('body');
 function showSearch(event) {
-   //img.classList.toggle('active');
-   search.classList.toggle("active");
+  //img.classList.toggle('active');
+  search.classList.toggle("active");
 }
-
-
-
-
-
-
 
 console.log(window.scrollY);
 
 document.addEventListener("DOMContentLoaded", function sleep() {
-   console.log(window.scrollY);
-   const arrowUp = document.querySelector(".arrow-up");
-   console.log("1233");
-   window.addEventListener("scroll", function () {
-
-      if (window.scrollY > 200) {
-         arrowUp.style.opacity = "1";
-         arrowUp.style.transition = "1s";
-         console.log("1233");
-      } else {
-         arrowUp.style.transition = "1s";
-         arrowUp.style.opacity = "0";
-         console.log("0987");
-      }
-   });
-   // setTimeout(sleep, 1000);
+  console.log(window.scrollY);
+  const arrowUp = document.querySelector(".arrow-up");
+  console.log("1233");
+  window.addEventListener("scroll", function () {
+    if (window.scrollY > 200) {
+      arrowUp.style.opacity = "1";
+      arrowUp.style.transition = "1s";
+      console.log("1233");
+    } else {
+      arrowUp.style.transition = "1s";
+      arrowUp.style.opacity = "0";
+      console.log("0987");
+    }
+  });
+  // setTimeout(sleep, 1000);
 });
-
